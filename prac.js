f107933ea5eb7d88c563e9268b2d6413ddc0e1bd
karthikabhaskar <karthika.b@appinessworld.com>
2022-07-18 12:34:19 +0530

    changed

---
+++ prac.js
@@ -0,0 +1,26 @@
+// let arr = [[10, 0, 100,10], [0, 100, 10,100], [10, 50, 100,15], [50, 100, 10,55], [0, 100, 10,25],[10,100,100,50]];
+// console.log(arr);
+// let arr1 = [];
+
+// for (let i = 0; i < arr[0].length; i++) {
+//     let sum = 0;
+//     for (let j = 0; j < arr.length; j++) {
+//         sum = sum + arr[j][i];
+
+//     }
+//     arr1.push(sum);
+// }
+// console.log(arr1)
+
+for (var i=0; i<3;i++){
+    setTimeout(()=> console.log(i),1);
+  }
+  
+  for (let i=0; i<3;i++){
+    setTimeout(()=> console.log(i),1);
+  }
+
+
+
+
+
